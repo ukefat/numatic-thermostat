14a39b714b9b05f4b10a455256ddd916b47ff5a9
ukefat <ukefat@gmail.com>
2025-03-26 01:21:10 -0600

    lowered packetbuffer pool size

--- Middlewares/Third_Party/connectedhomeip/src/platform/CHIPDeviceBuildConfig.h
+++ Middlewares/Third_Party/connectedhomeip/src/platform/CHIPDeviceBuildConfig.h
@@ -88,7 +88,7 @@
  *
  * Reduce packet buffer pool size to 8 (default 15) to reduce ram consumption
  */
-#define CHIP_SYSTEM_CONFIG_PACKETBUFFER_POOL_SIZE 7
+#define CHIP_SYSTEM_CONFIG_PACKETBUFFER_POOL_SIZE 5
 
 #define CHIP_DEVICE_CONFIG_ENABLE_WIFI_TELEMETRY 0
 #define CHIP_DEVICE_CONFIG_ENABLE_THREAD_TELEMETRY 0
